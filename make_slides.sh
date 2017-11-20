b07913ea3cca9bf545e411e07fbc50b03121d4d5
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2017-11-21 00:57:47 +0100

    Update index

--- make_slides.sh
+++ make_slides.sh
@@ -3,6 +3,9 @@
 set -x
 
 notebooks="tp-gitlab-ci.ipynb"
-jupyter-nbconvert $notebooks --to slides --output-dir=public
+revealprefix="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0"
+
+jupyter-nbconvert $notebooks --to slides --reveal-prefix $revealprefix --output-dir=public
 jupyter-nbconvert index.ipynb --to html --output-dir=public
 cp *.ipynb public/
+cp *.png public/
