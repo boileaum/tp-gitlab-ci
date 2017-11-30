78acbfb860474448a4281d164e3ad1d3bb8ef283
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2017-11-30 11:22:37 +0100

    Update make_* scripts

--- scripts/make_archive.sh
+++ scripts/make_archive.sh
@@ -3,7 +3,8 @@
 rm -rf archive
 mkdir -p archive
 cp tp-gitlab-ci.ipynb archive/
-cp *.png archive/
+cp -r images archive/
+cp -r videos archive/
 tar cfvz archive.tar.gz archive
 mkdir -p public
 mv archive.tar.gz public/
--- scripts/make_slides.sh
+++ scripts/make_slides.sh
@@ -8,4 +8,5 @@ revealprefix="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0"
 jupyter-nbconvert $notebooks --to slides --reveal-prefix $revealprefix --output-dir=public
 jupyter-nbconvert index.ipynb --to html --output-dir=public
 cp *.ipynb public/
-cp *.png public/
+cp -r images public/
+cp -r videos public/
